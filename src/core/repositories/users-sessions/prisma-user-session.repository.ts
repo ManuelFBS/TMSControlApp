a788a73d83323b5a2e4f22f025b0c237e0160379
ManuelFBS <manuelf.borrego@gmail.com>
2025-07-14 14:30:48 -0400

    Creac. 'prisma-user-session.repository.ts'...

---
+++ src/core/repositories/users-sessions/prisma-user-session.repository.ts
@@ -0,0 +1,13 @@
+import { UserSession } from '../../../core/entities/users-sessions/user-session.entity';
+
+export abstract class UserSessionRepository {
+        abstract create(user: UserSession): Promise<UserSession>;
+        abstract findAll(): Promise<UserSession[]>;
+        abstract findByDNI(dni: string): Promise<UserSession | null>;
+        abstract findByUsername(username: string): Promise<UserSession | null>;
+        abstract updateSessionEnd(
+                dni: string,
+                finalDate: Date,
+                finalHour: string,
+        ): Promise<UserSession>;
+}
