35ae6d6f3ab79f581fc69e69270919e7c2dc1792
Manuel F. Borrego S. <manuelf.borrego@gmail.com>
2025-07-14 01:07:59 -0400

    Creac. de 'employee.module.ts'...

---
+++ src/modules/employees/employee.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { EmployeeService } from '../../application/use-cases/employees/employee.service';
+import { EmployeeController } from '../../infrastructure/interface-adapters/employees/employee.controller';
+import { PrismaEmployeeRepository } from '../../infrastructure/repositories/employees/prisma-employee.repository';
+import { PrismaService } from '../../infrastructure/database/prisma/prisma.service';
+
+@Module({
+        controllers: [EmployeeController],
+        providers: [
+                EmployeeService,
+                PrismaService,
+                {
+                        provide: 'EmployeeRepository',
+                        useClass: PrismaEmployeeRepository,
+                },
+        ],
+        exports: [EmployeeService, 'EmployeeRepository'],
+})
+export class EmployeeModule {}
