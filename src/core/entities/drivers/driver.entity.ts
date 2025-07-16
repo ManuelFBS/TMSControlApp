81ca96fb80acd5720526ebd0a8193b84c424e2ec
ManuelFBS <manuelf.borrego@gmail.com>
2025-07-16 11:42:45 -0400

    Creac. 'driver.entity.ts'; Correcc. y/0 Modif. 'transport_company.entity.ts'...

---
+++ src/core/entities/drivers/driver.entity.ts
@@ -0,0 +1,8 @@
+export class Driver {
+        constructor(
+                public id: number,
+                public dni: string,
+                public driverLicense: string,
+                public idCompany: string,
+        ) {}
+}
--- src/core/entities/transport-company/transport_company.entity.ts
+++ src/core/entities/transport-company/transport_company.entity.ts
@@ -1,6 +1,7 @@
 export class TransportCompany {
         constructor(
                 public id: number,
+                public idCompany: string,
                 public companyName: string,
                 public contactName: string,
                 public contactPhone: string,
