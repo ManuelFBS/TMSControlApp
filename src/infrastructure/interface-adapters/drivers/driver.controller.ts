bba00e8dde0ca917f041009cb74c37cb6147bfa4
Manuel F. Borrego S. <manuelf.borrego@gmail.com>
2025-07-21 23:12:59 -0400

    Creac. 'driver.controller.ts'...

---
+++ src/infrastructure/interface-adapters/drivers/driver.controller.ts
@@ -0,0 +1,37 @@
+import {
+        Controller,
+        Get,
+        Post,
+        Body,
+        Param,
+        Put,
+        Delete,
+        UseGuards,
+        ParseIntPipe,
+} from '@nestjs/common';
+import { DriverService } from '../../../application/use-cases/drivers/driver.service';
+import {
+        CreateDriverDTO,
+        DriverResponseDTO,
+} from '../../../application/dto/drivers/create-driver.dto';
+import { JWTAuthGuard } from '../../../auth/guards/jwt-auth.guard';
+import { Permissions } from '../../../core/permissions/permissions.decorator';
+import { PermissionsGuard } from '../../../auth/guards/permissions.guard';
+import { plainToInstance } from 'class-transformer';
+
+@Controller('api/drivers')
+export class DriverController {
+        constructor(private readonly driverService: DriverService) {}
+
+        @Post('/driver/new')
+        @UseGuards(JWTAuthGuard, PermissionsGuard)
+        @Permissions('employee:create')
+        async create(
+                @Body() createDriverDTO: CreateDriverDTO,
+        ): Promise<DriverResponseDTO> {
+                const driver =
+                        await this.driverService.createDriver(createDriverDTO);
+
+                return plainToInstance(DriverResponseDTO, driver);
+        }
+}
